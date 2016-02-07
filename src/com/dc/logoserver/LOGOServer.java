d2059f662f90a945151f2de4eef016079bc761e7
Danny <Danny@Danny-Laptop.Home>
2016-02-07 01:23:17 +0000

    Send signal to GPIOs for motor

--- src/com/dc/logoserver/LOGOServer.java
+++ src/com/dc/logoserver/LOGOServer.java
@@ -7,16 +7,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
 
 public class LOGOServer {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(83);
 		boolean listen = true;
+
+		GpioController gpio = GpioFactory.getInstance();
+		GpioPinDigitalOutput in1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_23);
+		GpioPinDigitalOutput in2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27);
+		GpioPinDigitalOutput in3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28);
+		GpioPinDigitalOutput in4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_29);
+		Map<String, GpioPinDigitalOutput> pins = new HashMap<String, GpioPinDigitalOutput>();
 
 		while (listen) {
 			Socket connection = serverSocket.accept();
 
-			// TODO: Turn green LED on
 			BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
 			InputStreamReader isr = new InputStreamReader(is);
 			StringBuffer process = new StringBuffer();
@@ -26,32 +39,69 @@ public class LOGOServer {
 				process.append((char) character);
 			}
 
-			// TODO: Parse command. If valid, run motors. Otherwise, flash red
-			// LED and send failure response to client
 			String input = process.toString().toLowerCase();
 			String response;
 
-			if (input.equalsIgnoreCase("green on")) {
-				// TODO: turn green led on
-				response = "Green LED successfully switched on";
-			} else if (input.equalsIgnoreCase("green off")) {
-				// TODO: turn green led off
-				response = "Green LED successfully switched off";
-			} else if (input.equalsIgnoreCase("red on")) {
-				// TODO: turn red led on
-				response = "Red LED successfully switched on";
-			} else if (input.equalsIgnoreCase("red off")) {
-				// TODO: turn red led off
-				response = "Red LED successfully switched off";
-			} else if (input.matches("^((fd|rt|lt) [0-9]+;)+x$")) {
+			if (input.matches("^((fd|rt|lt) [0-9]+;)+x$")) {
 				int lastSemiColon = input.lastIndexOf(";");
 				String[] commands = input.substring(0, lastSemiColon).split(";");
 
 				for (String command : commands) {
 					System.out.println("Executing command: " + command);
+					int speed = 1;
+					int distance = Integer.parseInt(command.split(" ")[1]) * 100;
+
+					// Make sure all inputs are off before we send the signal
+					in1.low();
+					in2.low();
+					in3.low();
+					in4.low();
+
+					in4.high();
+					for (int x = 0; x < distance; x++) {
+						System.out.println("Loop " + x);
+						Thread.sleep(speed);
+						in3.high();
+						Thread.sleep(speed);
+						in4.low();
+						Thread.sleep(speed);
+						in2.high();
+						Thread.sleep(speed);
+						in3.low();
+						Thread.sleep(speed);
+						in1.high();
+						Thread.sleep(speed);
+						in2.low();
+						Thread.sleep(speed);
+						in4.high();
+						Thread.sleep(speed);
+						in1.low();
+					}
+
+					// Make sure all inputs are off after we send the signal
+					in1.low();
+					in2.low();
+					in3.low();
+					in4.low();
 				}
 
 				response = "LOGO commands successfully executed";
+			} else if (input.matches("^[0-9]{1,2};x$")) {
+				int lastSemiColon = input.lastIndexOf(";");
+				String pinNumber = input.substring(0, lastSemiColon);
+				String pinName = "GPIO " + Integer.parseInt(pinNumber);
+				GpioPinDigitalOutput pin;
+
+				if (pins.containsKey(pinName)) {
+					pin = pins.get(pinName);
+				} else {
+					pin = gpio.provisionDigitalOutputPin(RaspiPin.getPinByName(pinName));
+				}
+
+				pin.toggle();
+
+				response = pinName + " toggled successfully";
+				System.out.println(response);
 			} else {
 				response = "Invalid input: " + input;
 			}
@@ -60,8 +110,6 @@ public class LOGOServer {
 			OutputStreamWriter osw = new OutputStreamWriter(os);
 			osw.write(response + (char) 13);
 			osw.flush();
-
-			// TODO: Turn green LED off
 		}
 
 		serverSocket.close();
